962f559e38e66aec94832c8555fb5043c62d789c
Chris Weston <william.c.weston1@gmail.com>
2025-07-20 23:09:27 -0400

    Removing weston_utils/ folder and all associated files

--- weston_utils/__init__.py
+++
@@ -1,2 +0,0 @@
-# utils/__init__.py
-
--- weston_utils/logging_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# weston_utils/logging_utils/__init__.py
-
-# Logging utils
-from .configure_logging import configure_logging
-
-__all__ = [
-    "configure_logging"
-]
